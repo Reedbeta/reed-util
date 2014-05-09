f8c569d7b7e244d2652d8712ce93633f21f1472c
Nathan Reed <nathaniel.reed@gmail.com>
2014-05-09 14:52:25 -0700

    Make util-rng mostly inline instead of hidden behind the .cpp

--- util-rng.h
+++ util-rng.h
@@ -1,21 +1,56 @@
 #pragma once
+#include "util.h"
 
 namespace util
 {
-	// Thomas Wang's fast 32-bit integer hash
-	uint wangHash(uint x);
+	// Thomas Wang's integer hash, as reported by Bob Jenkins
+	inline uint wangHash(uint x)
+	{
+		// Thomas Wang's integer hash, as reported by Bob Jenkins
+		x = (x ^ 61) ^ (x >> 16);
+		x *= 9;
+		x = x ^ (x >> 4);
+		x *= 0x27d4eb2d;
+		x = x ^ (x >> 15);
+		return x;
+	}
 
 	// Fast RNG using Xorshift algorithm
 	struct RNG
 	{
 		uint m_state;
 
-		void seed(uint seed);
-		void seed();
+		RNG() { seedWithTime(); }
+		explicit RNG(uint seed_): m_state(wangHash(seed_)) {}
 
-		uint randUint();
-		int randInt(int min, int max);
-		float randFloat();
-		float randFloat(float min, float max);
+		void seedWithTime();
+		void seed(uint seed_) { m_state = wangHash(seed_); }
+
+		uint randUint()
+		{
+			// Xorshift algorithm from George Marsaglia's paper
+			m_state ^= (m_state << 13);
+			m_state ^= (m_state >> 17);
+			m_state ^= (m_state << 5);
+			return m_state;
+		}
+
+		int randInt(int min, int max)
+		{
+			// Note: this doesn't give a uniform distribution if |max - min|
+			// isn't a divisor of 2^32.  Problems will be more apparent the
+			// larger |max - min| is.  Could use rejection sampling to fix.
+			return min + (randUint() % (max - min));
+		}
+
+		float randFloat()
+		{
+			return float(randUint()) * (1.0f / 4294967296.0f);
+		}
+
+		float randFloat(float min, float max)
+		{
+			return lerp(min, max, randFloat());
+		}
 	};
 }
