7bfb2245c960b66c6ba2d05b365e50e6b40fb2a1
Nathan Reed <nathaniel.reed@gmail.com>
2016-08-09 12:25:05 -0700

    Add basic array type that holds a pointer and size (with the actual data stored elsewhere).

--- test/test-containers.cpp
+++ test/test-containers.cpp
@@ -1,6 +1,22 @@
 #include "util-containers.h"
 #include <cstdio>
 
+// (actually only tests compilation, not results...)
+// !!!UNDONE: proper tests
+
 void TestContainers()
 {
+	using namespace util;
+
+	array<int> a1;
+	array<int> a2 = { nullptr, 0 };
+	array<const int> a3 = { 1, 2, 3, 4, 5 };	// points to initializer list's contents
+
+	struct A {};
+	struct B : public A {};
+	array<const B> a4 = { B{}, B{}, B{} };
+	array<const A> a5 = a4;		// implicit conversion from B* to A*
+
+	int C_array[] = { 8, 9, 10, 11, 12 };
+	array<int> a6 = C_array;	// infers size known at compile time
 }
--- util-containers.h
+++ util-containers.h
@@ -1,6 +1,28 @@
 #pragma once
 #include "util-basics.h"
+#include <initializer_list>
 
 namespace util
 {
+	// Basic array type: not really a container, just a pointer and size,
+	// with the actual data stored elsewhere.
+	template <typename T>
+	struct array
+	{
+		T *		data;
+		size_t	size;
+
+		// Subscript accessor
+		T & operator[] (int i) { return data[i]; }
+
+		// Constructors
+		array(): data(nullptr), size(0) {}
+		array(T * data_, size_t size_): data(data_), size(size_) {}
+		array(std::initializer_list<T> initList): data(&(*initList.begin())), size(initList.size()) {}
+		template <typename U> array(array<U> a): data(a.data), size(a.size) {}
+		template <typename U, size_t N> array(U(& a)[N]): data(a), size(N) {}
+
+		// Create a "view" of a sub-range of the array
+		array<T> slice(size_t start, size_t slice_size) { return { data + start, slice_size }; }
+	};
 }
