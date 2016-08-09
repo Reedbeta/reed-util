4ef00ed71289716b3a0e09237c33aa79e6f422ee
Nathan Reed <nathaniel.reed@gmail.com>
2016-08-08 23:16:11 -0700

    Reorganize project with a separate test executable. Move existing math test code into the new exe.

---
+++ test/test.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main (int /*argc*/, const char ** /*argv*/)
+{
+	// !!!UNDONE: command line processing to select subset of tests to run
+
+	//TestMath();	// not yet implemented
+	//TestRNG();	// not yet implemented
+
+	return 0;
+}
